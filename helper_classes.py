c90aa81b693d1201b779c409c20fea99953f3114
Sh1tters <72045321+Sh1tters@users.noreply.github.com>
2022-09-07 13:19:34 +0200

    Made calibration for path value

--- helper_classes.py
+++ helper_classes.py
@@ -1,6 +1,7 @@
 from pybricks.robotics import DriveBase
 from pybricks.ev3devices import ColorSensor
 from pybricks.tools import wait
+import json
 
 class LineFollower:
     def __init__(self, robot: DriveBase, line_sensor: ColorSensor, path_value: int, wall_value: int, accepted_deviance, turn_angle: int, drive_speed: int):
@@ -58,3 +59,46 @@ class LineFollower:
             else:
                 self.robot.drive(self.drive_speed, 0)
                 wait(10)
+
+class Calibration:
+    def __init__(self, robot: DriveBase, ev3: EV3Brick, line_sensor: ColorSensor, lf: LineFollower) -> None:
+        self.robot = robot
+        self.ev3 = ev3
+        self.line_sensor = line_sensor
+        self.lf = lf
+        self.calibrated = False
+
+    def run(self) -> None:
+        while not self.calibrated:
+            self.ev3.screen.print('Calibrating...')
+            pv1 = self.line_sensor.reflection()
+            wait(200)
+            
+            self.robot.straight(50)
+            pv2 = self.line_sensor.reflection()
+            wait(200)
+
+            self.robot.straight(-40)
+            pv3 = self.line_sensor.reflection()
+            wait(200)
+
+            path_value = int((pv1 + pv2 + pv3) / 3)
+            wait(800)
+            self.ev3.screen.clear()
+            self.ev3.screen.print(f'Found path value {path_value}')
+            wait (500)
+
+            # change settings.py path values
+            jsonDump = json.dumps({"PATH_VALUE", path_value})
+            jsonFile = open("config.json", "w")
+            jsonFile.write(jsonDump)
+            jsonFile.close()
+            self.calibrated = True
+
+            #Calibration DONE
+            self.ev3.screen.clear()
+            self.ev3.screen.print(f'Calibration done')
+
+            # Run linefollower
+            self.lf.run()
+
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pybricks.robotics import DriveBase
 
 from settings import PATH_VALUE, WALL_VALUE, ACCEPTED_DEVIANCE, TURN_ANGLE, DRIVE_SPEED
 
-from helper_classes import LineFollower
+from helper_classes import LineFollower, Calibration
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
@@ -21,6 +21,9 @@ line_sensor = ColorSensor(Port.S3)
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-# Make and Run Line Follower
+# Make Line Follower
 lf = LineFollower(robot, line_sensor, PATH_VALUE, WALL_VALUE, ACCEPTED_DEVIANCE, TURN_ANGLE, DRIVE_SPEED)
-lf.run()
+
+# Make Calibration
+cal = Calibration(robot, ev3, line_sensor, lf)
+cal.run()

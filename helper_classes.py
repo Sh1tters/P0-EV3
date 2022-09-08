49520ed5723d834317a3996b3fedb39fbb55eb02
ReturnOfTheLast <returnofthelast@gmail.com>
2022-09-08 08:52:42 +0200

    Added docstring to LineFollower

--- helper_classes.py
+++ helper_classes.py
@@ -2,8 +2,19 @@ from pybricks.robotics import DriveBase
 from pybricks.ev3devices import ColorSensor
 from pybricks.tools import wait
 
-class LineFollower:
+class LineFollower:  
     def __init__(self, robot: DriveBase, line_sensor: ColorSensor, path_value: int, wall_value: int, accepted_deviance, turn_angle: int, drive_speed: int):
+        """__init__ Constructs the necessary variables and objects to build a line follower
+
+        Args:
+            robot (DriveBase): The robot DriveBase
+            line_sensor (ColorSensor): The sensor to detect the line
+            path_value (int): Reflection value of the line
+            wall_value (int): Reflection valie of the walls
+            accepted_deviance (_type_): The accepted deviance/change in reflection
+            turn_angle (int): The angle of which we want to turn
+            drive_speed (int): The speed we want to drive
+        """        
         self.robot = robot
         self.line_sensor = line_sensor
         self.path_value = path_value
@@ -14,15 +25,32 @@ class LineFollower:
         self.shut_down = False
     
     def isOnPath(self) -> bool:
+        """isOnPath Check if robot is on path
+
+        Returns:
+            bool: Is on path
+        """        
         return self.path_value + self.accepted_deviance > self.line_sensor.reflection() > self.path_value - self.accepted_deviance
 
     def isOnWall(self) -> bool:
+        """isOnWall Check if robot is on a wall
+
+        Returns:
+            bool: Is on wall
+        """        
         return self.wall_value + self.accepted_deviance > self.line_sensor.reflection() > self.wall_value - self.accepted_deviance
 
     def isOffPath(self) -> bool:
+        """isOffPath Check if robot is off path
+
+        Returns:
+            bool: Is off path
+        """        
         return not self.isOnPath() and not self.isOnWall()
 
     def autocorrectPath(self) -> None:
+        """autocorrectPath Autocorrect to the path
+        """        
         self.robot.stop()
 
         swing_multiplier = 1
@@ -44,10 +72,9 @@ class LineFollower:
             
             direction *= -1
 
-    def adjust_lane(self) -> None:
-        pass
-
     def run(self) -> None:
+        """run Run the Line Following
+        """        
         # Run Loop
         while not self.shut_down:
             if self.isOnWall():
